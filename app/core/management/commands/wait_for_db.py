86125ceea00ceb3c043efbe3008dbff4f39b3459
Jadiel <teofilojadiel@gmail.com>
2021-04-17 17:27:16 -0300

    add db connection and user app

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,19 @@
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        db_con = None
+        while not db_con:
+            try:
+                db_con = connections['default']
+            except OperationalError:
+                time.sleep(1)
+
+
+
--- app/core/models.py
+++ app/core/models.py
@@ -25,9 +25,9 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that upports using email instead of username"""
+    """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.EmailField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
