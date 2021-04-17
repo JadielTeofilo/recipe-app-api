e4356679b67da0636f112eaaf7b400d1ff041f32
Jadiel <teofilojadiel@gmail.com>
2021-04-17 17:47:57 -0300

    Add token endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -40,3 +41,34 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_for_user(self):
+        payload = {
+            'email': 'asdf@asdf.com',
+            'password': 'asdfsdfg',
+            'name': 'asdf',
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        create_user(email='asdf@asdf.com', password='asdf')
+        payload = {
+            'email': 'asdf@asdf.com',
+            'password': 'asdfsdfg',
+            'name': 'asdf',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+
+    def test_create_token_no_user(self):
+        payload = {
+            'email': 'asdf@asdf.com',
+            'password': 'asdfsdfg',
+            'name': 'asdf',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+
